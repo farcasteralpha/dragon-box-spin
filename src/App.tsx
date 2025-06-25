afaa945d788f170f520e467de927b5414a4dc966
farcasteralpha <babyanon07@gmail.com>
2025-06-25 08:08:40 +0700

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,14 @@
-import { sdk } from "@farcaster/frame-sdk";
-import React, { useEffect, useState } from "react";
-import { useAccount, useConnect, useSignMessage } from "wagmi";
+import { NeynarProvider } from '@neynar/react-mini';
+import GameBox from './GameBox';
 
 function App() {
-  useEffect(() => {
-    sdk.actions_ready();
-  }, []);
-
   return (
-    <div className="container">
-      <h1>🎮 Dragon Box: Tap to Claim</h1>
-      <ConnectMenu />
-    </div>
-  );
-}
-
-function ConnectMenu() {
-  const { isConnected, address } = useAccount();
-  const { connect, connectors } = useConnect();
-
-  if (isConnected) {
-    return (
-      <>
-        <div>Connected account:</div>
-        <div>{address}</div>
-        <SignButton />
-      </>
-    );
-  }
-
-  return (
-    <button type="button" onClick={() => connect({ connector: connectors[0] })}>
-      Connect Wallet
-    </button>
-  );
-}
-
-function SignButton() {
-  const { signMessage, isPending, data, error } = useSignMessage();
-  const [claimed, setClaimed] = useState(false);
-
-  const handleSign = () => {
-    signMessage({ message: "I want to claim the reward!" });
-    setClaimed(true);
-  };
-
-  if (claimed) {
-    return <div className="claimed">🎉 Successfully Claimed!</div>;
-  }
-
-  return (
-    <button type="button" onClick={handleSign} disabled={isPending}>
-      {isPending ? "Signing..." : "Tap to Claim"}
-    </button>
+    <NeynarProvider>
+      <GameBox />
+    </NeynarProvider>
   );
 }
 
 export default App;
+
 
